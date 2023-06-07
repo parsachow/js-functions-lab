e4797e4291340feb5c41f3dec1459b9f43c58da4
parsachow <parsakishwa06@gmail.com>
2023-06-07 19:02:15 -0400

    solved till Question 5

--- app.js
+++ app.js
@@ -31,9 +31,44 @@ console.log(maxOfThree(7, 13, 10));
 
 //Define a function, as a function declaration, isCharAVowel that takes a character as an argument and returns true if it is a vowel, false otherwise.
 
+function isCharAVowel(vowel) {
+    if (vowel === "A"){
+      return true;
+    }else if (vowel === "E") {
+      return true;
+    }else if (vowel === "I") {
+      return true;
+    }else if (vowel === "O") {
+      return true;
+    }else if (vowel === "U") {
+      return true;
+    }else {
+      return false;
+    }
+}
+console.log(isCharAVowel('x'));
+
 //Define a function, as a function expression, sumArray that takes an array of numbers and returns the sum of those numbers. For example, sumArray([2, 4, 5]); would return 11.
 
+const sumArray = function(a) {
+    let sum = 0
+  a.forEach(function(num){
+    sum = sum + num
+  })
+  return sum;
+} 
+console.log(sumArray([5, 10, 15, 20]));
+
 //Define a function, as a function declaration, multiplyArray that takes an array of numbers and returns the product those numbers. For example, multiplyArray([2, 4, 5]); would return 40.
+
+function multiplyArray(m) {
+    let multiply = 1
+    m.forEach (function(num) {
+        multiply = multiply * num
+    })
+    return multiply;
+    }
+    console.log(multiplyArray([2, 5, 10]));
 
 //Define a function, as a function expression, numArgs that returns the number of arguments passed to the function when called.
 
